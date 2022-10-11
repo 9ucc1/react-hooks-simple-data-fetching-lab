11b5fda4ef49bcf77641fe4b06e147773cab449a
Gucci Fan <gucc@umich.edu>
2022-10-10 21:58:53 -0400

    commit tree 1803344b70bd9209f45a531c4388e09cbfbf1d95

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, {useState, useEffect} from 'react'
+
+function App(){
+
+    const [image, setImage] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response)=>response.json())
+        .then((response)=>{
+            setImage(response.message)
+            setIsLoaded(true)
+        })
+    },[])
+
+    if (!isLoaded){
+        return(
+            <p>Loading...</p>
+        )
+    }
+
+    return (
+        <img src={image} alt="A Random Dog"></img>
+    )
+}
+
+export default App
